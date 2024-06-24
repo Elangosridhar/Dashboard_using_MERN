e49b4a8441785007461266fd3904052778afde1e
Elangosridhar <elangosridhar11@gmail.com>
2024-06-24 23:38:18 +0530

    second

--- src/Controller/auth/employeeregister.js
+++ src/Controller/auth/employeeregister.js
@@ -1,18 +1,19 @@
 import { Employee } from "../../Models/employeeModel.js";
+import bcrypt from "bcrypt";                  
 
 export const employeeregister = async (req, res) => {
     try {
-        const {employee_email} = req.body
+        const {employee_email,password} = req.body
         const findperson=await Employee.findOne({employee_email})
         if(!findperson){
+            const hashpasswword =await bcrypt.hash(password, 10);
+            console.log(hashpasswword)
             const newEmployee = await Employee(req.body).save();
             res.send({ message: "Successfully registered", employee: newEmployee });
         }else{
            res.send({message:"Already email exists"})  
         }
-       
-
-        
+    
     } catch (err) {
         console.log(err);
         res.send({ error: "An error occurred during registration." });
--- src/Controller/auth/longinController.js
+++ src/Controller/auth/longinController.js
@@ -1,18 +1,26 @@
 import { Employee } from "../../Models/employeeModel.js";
 import jwt from 'jsonwebtoken';
+import bcrypt from "bcrypt"; 
 
 
 export const login = async (req,res) =>{
     try{
         console.log(req.body)
-        const resdata=await Employee.findOne(req.body);
+        const {employee_email,password} = req.body
+        const resdata=await Employee.findOne({});
        if(resdata){
-           const token = jwt.sign({employee_email:resdata.employee_email,employee_name:resdata.employee_name,role:resdata.role},"ATPRO2024")
-           res.send({message:"successfully logged in",token:token})
-       }
+           const checkpassword =await bcrypt.compare(password, resdata.password) 
+         if (checkpassword){
+            const token = jwt.sign({employee_email:resdata.employee_email,employee_name:resdata.employee_name,role:resdata.role},"ATPRO2024")
+            res.send({message:"successfully logged in",token:token})
+        }
        else{
             res.send({message:"Invalid email or password"})
        }
+    }
+    else{
+        res.send({message:"Invalid email or password"})
+    }
        
     }
     catch(err){
