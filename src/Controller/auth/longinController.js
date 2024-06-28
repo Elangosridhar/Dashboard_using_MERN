abaaf69d37d35eec0b9765705691026a066fecce
Elango <elangosridhar11@gmail.com>
2024-06-29 03:44:29 +0530

    update

--- src/Controller/auth/longinController.js
+++ src/Controller/auth/longinController.js
@@ -3,15 +3,21 @@ import jwt from 'jsonwebtoken';
 import bcrpt from "bcrypt"; 
 
 
-export const login = async (req, res) => {
+export const login =  async (req, res) => {
     try{
         console.log(req.body)
-        // const {employee_email,password} = req.body;
-        const resdata=await Employee.findOne(req.body);
+        const {employee_email,password} = req.body;
+        const resdata=await Employee.findOne({employee_email});
        if(resdata){
-        
-           const token = jwt.sign({employee_email:resdata.employee_email,employee_name:resdata.employee_name,role:resdata.role},"ATPRO2024")
-           return res.send({message:"successfully logged in",token})
+           const checkpassword = await bcrpt.compare(password, resdata.password)
+           console.log(checkpassword)
+           if(checkpassword){
+               const token = jwt.sign({employee_email:resdata.employee_email,employee_name:resdata.employee_name,role:resdata.role},"ATPRO2024")
+               return res.send({message:"successfully logged in",token})
+           }
+           else{
+               res.send({message:"Invalid email or password"})
+           }
        }
        else{
         res.send({message:"Invalid email or password"})
